b88b38dd6d9f0b81a030446138b2a0ffb66cc06c
Cristopher Jonas de Faria <cristopherjonasfaria@gmail.com>
2019-03-01 23:17:04 -0300

    counter test

---
+++ counter/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    contador: 0
+  }
+
+  increment = () => {
+    this.setState({ contador: this.state.contador + 1 })
+  }
+
+  decrement = () => {
+    this.setState({ contador: this.state.contador - 1 })
+  }
+
+  clear = () => {
+    this.setState({ contador: 0 })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Contador:{this.state.contador}
+          <button onClick={this.increment}>+</button>
+          <button onClick={this.decrement}>-</button>
+          <button onClick={this.clear}>clear</button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
